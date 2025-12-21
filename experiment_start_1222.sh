e01099dfa91ae50c098893287c778ae22a68344a
harry <1025527264@qq.com>
2025-12-22 00:14:49 +0800

    add qwen 235b thinking 2507

---
+++ experiment_start_1222.sh
@@ -0,0 +1,16 @@
+# !/usr/bin/env bash
+
+sweagent run-batch --config config/Qwen3_235BThinking2507_origin.yaml --agent.model.per_instance_cost_limit 10.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
+
+
+sleep 300
+
+sweagent run-batch --config config/Qwen3_235BThinking2507_agenticMemSearch_1220_13w.yaml --agent.model.per_instance_cost_limit 10.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
+
+#cd ../SWE-bench
+
+#python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Qwen3Coder30B_agenticMemSearch_1220_13w__qwen--qwen3-coder-30b-a3b-instruct__t-1.00__p-None__c-1.00___swe_bench_verified_test/preds.json --max_workers 20 --run_id validate-qwen3coder30b-agentmemsearch-1220-13w-top10
+
+#sleep 10
+
+#python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/dsv31t_origin__deepseek--deepseek-v3-1-terminus__t-1.00__p-None__c-1.00___swe_bench_verified_test/preds.json --max_workers 20 --run_id validate-dsv31t-origin-100-2
