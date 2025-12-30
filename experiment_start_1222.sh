3c8e565e4e402ecb7e06ab7789ff616a73ce4621
harry <1025527264@qq.com>
2025-12-30 15:34:38 +0800

    111add more models

--- experiment_start_1222.sh
+++ experiment_start_1222.sh
@@ -13,7 +13,9 @@ sweagent run-batch --config config/Qwen3Coder30B_agenticMemSearch_1220_13w.yaml 
 sweagent run-batch --config config/Qwen3_235BInstruct2507_agenticMemSearch_1220_13w.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :500 --instances.shuffle=False
 
 
+sweagent run-batch --config config/Qwen3Coder30B_agenticMemSearch_1220_13w_top1.yaml --agent.model.per_instance_cost_limit 0.50 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
 
+sweagent run-batch --config config/Qwen3Coder30B_agenticMemSearch_1220_13w_top5.yaml --agent.model.per_instance_cost_limit 0.50 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
 
 
 sleep 300
@@ -26,6 +28,16 @@ sweagent run-batch --config config/dsv31t_agenticMemSearch_1220_13w.yaml --agent
 sweagent run-batch --config config/Gemini3Flash_agenticMemSearch_1220_13w.yaml --agent.model.per_instance_cost_limit 5.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 1 --instances.slice :500 --instances.shuffle=False
 
 sweagent run-batch --config config/Qwen3_235BInstruct2507_origin.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :500 --instances.shuffle=False
+
+sweagent run-batch --config config/dsv31t_agentic_rag_top3.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :500 --instances.shuffle=False
+
+sweagent run-batch --config config/Qwen3Coder30B_agentic_rag_top3.yaml --agent.model.per_instance_cost_limit 0.30 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :500 --instances.shuffle=False
+
+sweagent run-batch --config config/dsv31t_agenticMemSearch_1220_13w_top1.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
+
+sweagent run-batch --config config/dsv31t_agenticMemSearch_1220_13w_top3.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
+
+sweagent run-batch --config config/dsv31t_agenticMemSearch_1220_13w_top5.yaml --agent.model.per_instance_cost_limit 1.00 --instances.type swe_bench --instances.subset verified --instances.split test --num_workers 20 --instances.slice :100 --instances.shuffle=False
 
 #cd ../SWE-bench
 
@@ -46,3 +58,13 @@ sweagent run-batch --config config/Qwen3_235BInstruct2507_origin.yaml --agent.mo
 # python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/KimiK2_Instruct0711_agenticMemSearch_1220_13w__moonshotai--kimi-k2__t-1.00__p-None__c-3.00___swe_bench_verified_test/preds.json --max_workers 20 --run_id validate-kimi-k2-agenticMemSearch-1220-13w-100
 
 # python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Gemini25flash_agenticMemSearch_1220_13w__google--gemini-2.5-flash__t-1.00__p-None__c-5.00___swe_bench_verified_test/preds.json --max_workers 20 --run_id validate-gemini25flash-agenticMemSearch-1220-13w-100
+
+# python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Qwen3_235BInstruct2507_agenticMemSearch_1220_13w__qwen--qwen3-235b-a22b-instruct-2507__t-1.00__p-None__c-0.30___swe_bench_verified_test/preds.json --max_workers 10 --run_id validate-qwen3_235BInstruct2507-agenticMemSearch-1220-13w-500-2
+
+# python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/ziming/SWE-bench/results/preds_claude4.json --max_workers 100 --run_id validate-claude4-agenticMemSearch-1220-13w-500
+
+# python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Qwen3Coder30B_agenticMemSearch_1220_13w_top1__qwen--qwen3-coder-30b-a3b-instruct__t-1.00__p-None__c-0.20___swe_bench_verified_test/preds.json --max_workers 10 --run_id validate-qwen3coder30b-agenticMemSearch-1220-13w-top1-100
+
+# python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Qwen3Coder30B_agenticMemSearch_1220_13w_top3__qwen--qwen3-coder-30b-a3b-instruct__t-1.00__p-None__c-0.20___swe_bench_verified_test/preds.json --max_workers 10 --run_id validate-qwen3coder30b-agenticMemSearch-1220-13w-top3-100
+
+# python -m swebench.harness.run_evaluation --dataset_name princeton-nlp/SWE-bench_Verified --predictions_path /home/harry/Issue-Mem-SWE-agent/trajectories/harry/Qwen3Coder30B_agenticMemSearch_1220_13w_top5__qwen--qwen3-coder-30b-a3b-instruct__t-1.00__p-None__c-0.20___swe_bench_verified_test/preds.json --max_workers 10 --run_id validate-qwen3coder30b-agenticMemSearch-1220-13w-top5-100
